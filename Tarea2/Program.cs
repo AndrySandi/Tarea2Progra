b9b21fd178b391e55de3f4fb6f0c0644d9208e12
Andrysandi <andrysandi890@gmail.com>
2022-10-03 17:13:50 -0600

    Segunda tarea de programación

---
+++ Tarea2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----------CÁLCULO DEL AGUINALDO----------");
+
+            string[] nombreEmpleado = new string[3];
+
+            nombreEmpleado[0] = "Jose";
+            nombreEmpleado[1] = "Carmen";
+            nombreEmpleado[2] = "Roberto";
+
+            float[] salarioEmpleados = new float[3];
+
+            salarioEmpleados[0] = 1300;
+            salarioEmpleados[1] = 1500;
+            salarioEmpleados[2] = 2000;
+
+            Aguinaldo(nombreEmpleado, salarioEmpleados);
+            
+        }
+
+        static void Aguinaldo(string[] nombre, float[] salario)
+        {
+
+            int n = 0;
+            foreach (string i in nombre)
+            {
+                Console.WriteLine(i +" " + salario[n]);
+                n++;
+         
+            }
+
+            Console.WriteLine("-----------------------------------------");
+
+            Console.WriteLine("El salario actual de " + nombre[0] + " " + "es de: " + salario[0]);
+            Console.WriteLine("El salario actual de " + nombre[1] + " " + "es de: " + salario[1]);
+            Console.WriteLine("El salario actual de " + nombre[2] + " " + "es de: " + salario[2]);
+
+            for (int i = 0; i < 3; i++)
+            {
+                salario[i] *= 12;
+                
+            }
+
+            Console.WriteLine("-----------------------------------------");
+
+            Console.WriteLine("El aguinaldo de " + nombre[0] + " " + "es de: " + salario[0]);
+            Console.WriteLine("El aguinaldo de " + nombre[1] + " " + "es de: " + salario[1]);
+            Console.WriteLine("El aguinaldo de " + nombre[2] + " " + "es de: " + salario[2]);
+
+            Console.WriteLine("-----------------------------------------");
+
+            Console.ReadKey();
+        }
+
+        
+        
+    }
+}
